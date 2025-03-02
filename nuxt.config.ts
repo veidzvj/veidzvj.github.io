337ff7944060f567719bb2f7982f49b824dcd1ca
Evrwaq <evrwaq@gmail.com>
2025-03-02 16:02:55 -0300

    style: add reset and variables css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: [
+    '~/assets/css/reset.css',
+    '~/assets/css/variables.css',
+    '~/assets/css/main.css',
+  ],
   modules: ['@nuxtjs/tailwindcss'],
 })
