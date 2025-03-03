749db80c2e99687c54becc62353542f6eb0b5797
Evrwaq <evrwaq@gmail.com>
2025-03-03 19:54:59 -0300

    style: add bottom border to header

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="bg-secundary flex items-center justify-center h-16">
-    <h1 class="text-secondary text-xl cursor-pointer font-logo">jvv</h1>
+  <div
+    class="bg-secundary flex items-center justify-center h-16 border-b border-b-primary border-solid"
+  >
+    <h1 class="text-secondary text-2xl cursor-pointer font-logo">jvv</h1>
   </div>
 </template>
