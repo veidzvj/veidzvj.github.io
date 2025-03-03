1544d1555b758d5bfd7c2d5170da14ba6a02153b
Evrwaq <evrwaq@gmail.com>
2025-03-03 19:49:07 -0300

    style: add custom styles to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: 'var(--bg-color)',
+        secundary: 'var(--bg-color-secundary)',
+        gray: 'var(--gray-color)',
+      },
+      textColor: {
+        primary: 'var(--white-color)',
+        secondary: 'var(--primary-color)',
+        gray: 'var(--gray-color)',
+      },
+      borderColor: {
+        primary: 'var(--primary-color)',
+      },
+      fontFamily: {
+        logo: ['Playwrite IT Moderna', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
-
