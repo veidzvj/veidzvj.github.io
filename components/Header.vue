60c045b166cf332f36a30829462ff6937c955f33
Evrwaq <evrwaq@gmail.com>
2025-03-04 20:06:28 -0300

    style: adjust header height

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-secundary flex items-center justify-center h-16 border-b border-b-primary border-solid"
+    class="bg-secundary flex items-center justify-center h-20 border-b border-b-primary border-solid"
   >
     <h1 class="text-secondary text-2xl cursor-pointer font-logo">jvv</h1>
   </div>
