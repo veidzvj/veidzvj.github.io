a3eb2cfa71ce782dc6a04d84ff6e220054043570
Evrwaq <evrwaq@gmail.com>
2025-03-04 22:46:43 -0300

    refactor: adjust Project component

--- components/Project.vue
+++ components/Project.vue
@@ -1,15 +1,17 @@
 <template>
-  <div
+  <a
+    :href="link"
+    target="_blank"
     class="border border-solid border-primary rounded w-48 h-40 flex flex-col justify-between p-4"
   >
     <div>
       <h1 class="text-lg">{{ name }}</h1>
       <p class="opacity-40 text-sm">{{ date }}</p>
     </div>
-    <a :href="link" target="_blank" class="cursor-pointer">
+    <div>
       <img src="~/assets/icons/github.svg" alt="GitHub" />
-    </a>
-  </div>
+    </div>
+  </a>
 </template>
 
 <script setup>
