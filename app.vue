7b3ce8ebbd91ea64b3894622c7b51b4df60606e0
Evrwaq <evrwaq@gmail.com>
2025-03-04 22:11:53 -0300

    feat: add Project component

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@
     <Header />
     <Profile />
     <Companies />
+    <Project />
   </div>
 </template>
 
@@ -16,4 +17,5 @@
 import Header from '~/components/Header.vue'
 import Profile from '~/components/Profile.vue'
 import Companies from '~/components/Companies.vue'
+import Project from '~/components/Project.vue'
 </script>
---
+++ components/Project.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="border border-solid border-primary rounded w-48 h-40 flex flex-col justify-between p-4"
+  >
+    <div>
+      <h1 class="text-lg">Project Name</h1>
+      <p class="opacity-40 text-sm">Description</p>
+    </div>
+    <a href="https://github.com/evrwaq" target="_blank" class="cursor-pointer">
+      <img src="~/assets/icons/github.svg" alt="GitHub" />
+    </a>
+  </div>
+</template>
