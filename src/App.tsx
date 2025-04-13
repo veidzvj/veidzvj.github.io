87961b4456f70538848463dd004f10574af738b4
Evrwaq <evrwaq@gmail.com>
2025-04-13 19:35:34 -0300

    feat: add Hero section and base constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import Hero from './sections/Hero'
+
 const App = () => {
   return (
-    <main>
-      <div>
-        <h1>Welcome to my Portfolio</h1>
-      </div>
-    </main>
+    <div>
+      <Hero />
+    </div>
   )
 }
 
---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+const words = [
+  { text: 'Ideas', imagePath: '/images/ideas.svg' },
+  { text: 'Concepts', imagePath: '/images/concepts.svg' },
+  { text: 'Designs', imagePath: '/images/designs.svg' },
+  { text: 'Code', imagePath: '/images/code.svg' },
+  { text: 'Ideas', imagePath: '/images/ideas.svg' },
+  { text: 'Concepts', imagePath: '/images/concepts.svg' },
+  { text: 'Designs', imagePath: '/images/designs.svg' },
+  { text: 'Code', imagePath: '/images/code.svg' },
+]
+
+export { words }
