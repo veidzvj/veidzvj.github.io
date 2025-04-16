db7957bb7905591baad2ae2718b4e9e0c961d0ae
Evrwaq <evrwaq@gmail.com>
2025-04-15 23:31:27 -0300

    feat: add base HeroExperience model component

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,4 +1,5 @@
 import Button from '../components/Button'
+import HeroExperience from '../components/HeroModels/HeroExperience'
 import { words } from '../constants'
 
 function Hero() {
@@ -46,6 +47,12 @@ function Hero() {
             />
           </div>
         </header>
+
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExperience />
+          </div>
+        </figure>
       </div>
     </section>
   )
