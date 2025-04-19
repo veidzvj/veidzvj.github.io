0f8c93bd963f9ffbf5e646912b5ba17b48f26b07
Evrwaq <evrwaq@gmail.com>
2025-04-19 18:12:19 -0300

    feat: add title animation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,8 +1,17 @@
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 import Button from '../components/Button'
 import HeroExperience from '../components/HeroModels/HeroExperience'
 import { words } from '../constants'
 
 function Hero() {
+  useGSAP(() => {
+    gsap.fromTo(
+      '.hero-text h1',
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: 'power2.inOut' }
+    )
+  })
   return (
     <section id="hero" className="relative overflow-hidden">
       <div className="absolute top-0 left-0 z-10">
