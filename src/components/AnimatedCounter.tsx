272b620c507b3da8e09327fb72393d5a3f787bf6
Evrwaq <evrwaq@gmail.com>
2025-04-21 19:49:15 -0300

    fix: adjust grid cols

--- src/components/AnimatedCounter.tsx
+++ src/components/AnimatedCounter.tsx
@@ -1,10 +1,10 @@
-import { counterItems } from '../constants'
 import CountUp from 'react-countup'
+import { counterItems } from '../constants'
 
 const AnimatedCounter = () => {
   return (
     <div id="counter" className="padding-x-lg xl:mt-0 mt-32">
-      <div className="mx-auto grid-cols-4">
+      <div className="mx-auto grid-4-cols">
         {counterItems.map((item) => (
           <div className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
             <div
