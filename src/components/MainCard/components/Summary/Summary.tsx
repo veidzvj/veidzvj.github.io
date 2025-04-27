a10b78c5fd743039cdbaeecb7479d912957c15b2
Evrwaqe <evrwaqe@gmail.com>
2025-04-26 22:19:54 -0300

    refactor: adjust Summary to include profile picture

--- src/components/MainCard/components/Summary/Summary.tsx
+++ src/components/MainCard/components/Summary/Summary.tsx
@@ -1,34 +1,40 @@
-import { FileText, Github, Linkedin } from 'lucide-react'
+import { SiLinkedin, SiGithub } from 'react-icons/si'
+import { FiFileText } from 'react-icons/fi'
 import './Summary.css'
 
 function Summary() {
   return (
     <div className="summary-container">
-      <div className="summary-title">
-        <h1>João Victor Veidz</h1>
-        <h2>Engenheiro de Software</h2>
-      </div>
-      <p className="summary-text">
-        Construindo sistemas robustos, escaláveis, testáveis e performáticos.
-      </p>
-      <div className="summary-links">
-        <a
-          href="https://github.com/evrwaqe"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Github size={24} />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/veidz/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Linkedin size={24} />
-        </a>
-        <a href="src/assets/cv.pdf" download>
-          <FileText size={24} />
-        </a>
+      <img
+        src="src/assets/profile.jpg"
+        alt="Foto de perfil de João Victor Veidz, engenheiro de software, usando óculos, barba curta, cabelo ondulado, com fundo de árvores verdes."
+      />
+
+      <div className="summary-right-container">
+        <div className="summary-title">
+          <h1>João Victor Veidz</h1>
+          <h2>Engenheiro de Software</h2>
+        </div>
+
+        <div className="summary-links">
+          <a
+            href="https://github.com/evrwaqe"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiGithub size={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/veidz/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiLinkedin size={24} />
+          </a>
+          <a href="src/assets/cv.pdf" download>
+            <FiFileText size={24} />
+          </a>
+        </div>
       </div>
     </div>
   )
