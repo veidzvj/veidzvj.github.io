f78a22d1a92e14127f68566f5d22a497dd3c025e
Evrwaqe <evrwaqe@gmail.com>
2025-04-26 22:30:40 -0300

    feat: add About component

--- src/components/MainCard/MainCard.tsx
+++ src/components/MainCard/MainCard.tsx
@@ -1,10 +1,12 @@
 import './MainCard.css'
+import About from './components/About/About'
 import Summary from './components/Summary/Summary'
 
 function MainCard() {
   return (
     <div className="main-card-container">
       <Summary />
+      <About />
     </div>
   )
 }
---
+++ src/components/MainCard/components/About/About.tsx
@@ -0,0 +1,18 @@
+import './About.css'
+
+function About() {
+  return (
+    <div className="about-container">
+      <h1>Sobre</h1>
+      <p>
+        Sou engenheiro de software com foco em desenvolvimento back end, meu
+        objetivo é criação de sistemas 100% testáveis, escaláveis, performáticos
+        e bem organizados. Sou adepto de arquiteturas e design patterns, e
+        sempre busco resolver problemas complexos da forma mais simples e
+        eficiente possível
+      </p>
+    </div>
+  )
+}
+
+export default About
