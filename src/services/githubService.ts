d2731b0a0ad55ea9623dc09609737446bbd54645
Evrwaqe <evrwaqe@gmail.com>
2025-04-28 01:02:21 -0300

    refactor: add cached repos logic

--- src/services/githubService.ts
+++ src/services/githubService.ts
@@ -5,13 +5,21 @@ export type Repo = {
   html_url: string
 }
 
+let cachedRepos: Repo[] | null = null
+
 async function fetchGithubRepos(): Promise<Repo[]> {
+  if (cachedRepos) {
+    return cachedRepos
+  }
+
   const response = await fetch(`https://api.github.com/users/evrwaqe/repos`)
   if (!response.ok) {
     throw new Error('Error searching for repos')
   }
 
   const data = await response.json()
+  cachedRepos = data
+
   return data
 }
 
